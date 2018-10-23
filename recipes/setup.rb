dbf87cfe6dee8f5f2c30fc094acf63564038d843
Rajneesh <rajneesh@mcmcg.com>
2018-10-23 14:21:48 +0000

    added system attributes to motd-raj file

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -13,7 +13,11 @@ package 'git' do
 end
 
 file '/etc/motd-raj' do
-	content 'This server is the property of MCMCG'
+	content "This server is the property of MCMCG
+		Hostname: #{node['hostname']}
+		IP addreess: #{node['ipaddress']}
+		CPU: #{node['cpu']['0']['mhz']}
+		Memory: #{node['memory']['total']}"
 	action :create
 	owner 'root'
 	group 'root'
